f5c7c5d6d0e2bb306da1ff82e17da333fa7c222e
manojworkhub <db9238422@gmail.com>
2024-06-15 09:16:58 +0530

    Updates

---
+++ static/js/blueprint-icons-all-paths-loader.ca4c84f1.chunk.js
@@ -0,0 +1,2 @@
+"use strict";(self.webpackChunkcrud=self.webpackChunkcrud||[]).push([[824],{161:(e,n,t)=>{t.r(n),t.d(n,{allPathsLoader:()=>s});var r=t(326),s=function(e,n){return(0,r.sH)(void 0,void 0,void 0,(function(){return(0,r.YH)(this,(function(r){switch(r.label){case 0:return[4,Promise.all([t.e(783),t.e(672),t.e(860)]).then(t.bind(t,780))];case 1:return[2,(0,r.sent().getIconPaths)(e,n)]}}))}))}}}]);
+//# sourceMappingURL=blueprint-icons-all-paths-loader.ca4c84f1.chunk.js.map
---
+++ static/js/blueprint-icons-all-paths.602af3e4.chunk.js
@@ -0,0 +1,2 @@
+"use strict";(self.webpackChunkcrud=self.webpackChunkcrud||[]).push([[860],{780:(c,n,e)=>{e.r(n),e.d(n,{IconSvgPaths16:()=>t,IconSvgPaths20:()=>u,getIconPaths:()=>s,iconNameToPathsRecordKey:()=>o});var r=e(902),t=e(378),u=e(413),a=e(455);function s(c,n){var e=(0,r.fL)(c);return n===a.l.STANDARD?t[e]:u[e]}function o(c){return(0,r.fL)(c)}}}]);
+//# sourceMappingURL=blueprint-icons-all-paths.602af3e4.chunk.js.map
